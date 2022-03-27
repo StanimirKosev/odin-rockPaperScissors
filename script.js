a20b928e2949f87d4dde66a299e102d58dfa4860
Stanimir <stanimirkosev00@gmail.com>
2022-03-27 16:02:21 +0300

    Computer play function done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function computerPlay(){
+    
+    randomNum = Math.floor(Math.random() * 3) +1;
+    
+    if (randomNum == 1){
+        randomHand = "Rock";
+    } 
+    else if (randomNum == 2){
+        randomHand = "Paper";
+    }
+    else {
+        randomHand = "Scissors";
+    }
+}
+
