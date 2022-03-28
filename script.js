9104d0e0956d5326b643569ed24a1429556a9843
Stanimir <stanimirkosev00@gmail.com>
2022-03-28 09:53:13 +0300

    Play one round function done

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
+/** Player 1 - Computer */
 function computerPlay(){
     
     randomNum = Math.floor(Math.random() * 3) +1;
     
     if (randomNum == 1){
-        randomHand = "Rock";
+        computerSelection = "rock";
     } 
     else if (randomNum == 2){
-        randomHand = "Paper";
+        computerSelection = "paper";
     }
     else {
-        randomHand = "Scissors";
+        computerSelection = "scissors";    
+    }
+    
+    return computerSelection;
+}  
+computerPlay();
+
+
+/** Player 2 - Human */
+playerSelection = prompt("Choose - Rock, Paper or Scissors").toLowerCase();
+
+
+
+function playRound(playerSelection,computerSelection){
+    
+    /**Wins */
+    if (playerSelection == "rock" && computerSelection == "scissors"){
+        return "You win! Rock beats scissors!"
+    }
+    if (playerSelection == "paper" && computerSelection == "rock"){
+        return "You win! Paper beats rock!"
+    }    
+    if (playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win! Scissors beats paper!"
+    }    
+    
+    /**Looses */
+    if (playerSelection == "scissors" && computerSelection == "rock"){
+        return "You lost! Rock beats scissors!"
+    }
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        return "You lost! Paper beats rock!"
+    }    
+    if (playerSelection == "paper" && computerSelection == "scissors"){
+        return "You lost! Scissors beats paper!"
+    }    
+    
+    /**Draw */
+    else {
+        return "It's a draw!"
     }
 }
 
+
+
+console.log(playRound(playerSelection,computerSelection));
